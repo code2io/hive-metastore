01debd62bf97acff5b17241af95d41deff232c5f
afurkanerdem <abdullahfurkanerdem@gmail.com>
2026-04-01 17:41:15 +0300

    Use apache/hive base image, simplify build

--- Dockerfile
+++ Dockerfile
@@ -1,30 +1,14 @@
-FROM eclipse-temurin:11-jre-alpine-3.23
+FROM apache/hive:standalone-metastore-4.1.0
 
-WORKDIR /opt
+USER root
 
-ENV HADOOP_VERSION=3.3.6
-ENV METASTORE_VERSION=4.1.0
-
-RUN apk add --no-cache netcat-openbsd curl bash
-
-ENV HADOOP_HOME=/opt/hadoop-${HADOOP_VERSION}
-ENV HIVE_HOME=/opt/apache-hive-metastore-${METASTORE_VERSION}-bin
-
-RUN curl -L https://archive.apache.org/dist/hive/hive-standalone-metastore-${METASTORE_VERSION}/hive-standalone-metastore-${METASTORE_VERSION}-bin.tar.gz | tar zxf - && \
-    curl -L https://archive.apache.org/dist/hadoop/common/hadoop-${HADOOP_VERSION}/hadoop-${HADOOP_VERSION}.tar.gz | tar zxf - && \
-    curl -L https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-8.0.19.tar.gz | tar zxf - && \
-    curl -L --output postgresql-42.4.0.jar https://jdbc.postgresql.org/download/postgresql-42.4.0.jar && \
-    cp mysql-connector-java-8.0.19/mysql-connector-java-8.0.19.jar ${HIVE_HOME}/lib/ && \
-    cp postgresql-42.4.0.jar ${HIVE_HOME}/lib/ && \
-    rm -rf  mysql-connector-java-8.0.19 && \
-    rm -rf  postgresql-42.4.0.jar
+RUN curl -L https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-8.0.19.tar.gz | tar zxf - && \
+    cp mysql-connector-java-8.0.19/mysql-connector-java-8.0.19.jar /opt/hive/lib/ && \
+    rm -rf mysql-connector-java-8.0.19 && \
+    curl -L --output /opt/hive/lib/postgresql-42.4.0.jar https://jdbc.postgresql.org/download/postgresql-42.4.0.jar
 
 COPY scripts/entrypoint.sh /entrypoint.sh
-
-RUN addgroup -g 1000 -S hive && \
-    adduser -S -u 1000 -G hive -h ${HIVE_HOME} hive && \
-    chown hive:hive -R ${HIVE_HOME} && \
-    chown hive:hive /entrypoint.sh && chmod +x /entrypoint.sh
+RUN chmod +x /entrypoint.sh
 
 USER hive
 EXPOSE 9083
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,42 +1,31 @@
 #!/bin/sh
 
-export HADOOP_HOME=/opt/hadoop-${HADOOP_VERSION:-3.3.6}
-export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-*.jar:${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-*.jar
-export JAVA_HOME=/usr/lib/jvm/default-jvm/jre
 export METASTORE_DB_HOSTNAME=${METASTORE_DB_HOSTNAME:-localhost}
 export METASTORE_TYPE=${METASTORE_TYPE:-mysql}
-
-HIVE_HOME=/opt/apache-hive-metastore-${METASTORE_VERSION:-4.1.0}-bin
 
 MYSQL='mysql'
 POSTGRES='postgres'
 
 if [ "${METASTORE_TYPE}" = "${MYSQL}" ]; then
   echo "Using mysql for metastore"
-  METASTORE_DB_PORT=${METASTORE_DB_PORT:-3306} # Default to 3306
+  METASTORE_DB_PORT=${METASTORE_DB_PORT:-3306}
   echo "Waiting for database on ${METASTORE_DB_HOSTNAME} to launch on ${METASTORE_DB_PORT} ..."
   while ! nc -z ${METASTORE_DB_HOSTNAME} ${METASTORE_DB_PORT}; do
     sleep 1
   done
-
   echo "Database on ${METASTORE_DB_HOSTNAME}:${METASTORE_DB_PORT} started"
-  echo "Init apache hive metastore on ${METASTORE_DB_HOSTNAME}:${METASTORE_DB_PORT}"
-
-  ${HIVE_HOME}/bin/schematool -initSchema -dbType mysql
-  ${HIVE_HOME}/bin/start-metastore
+  /opt/hive/bin/schematool -initSchema -dbType mysql
+  /opt/hive/bin/start-metastore
 fi
 
 if [ "${METASTORE_TYPE}" = "${POSTGRES}" ]; then
   echo "Using postgresql for metastore"
-  METASTORE_DB_PORT=${METASTORE_DB_PORT:-5432} # Default to 5432
+  METASTORE_DB_PORT=${METASTORE_DB_PORT:-5432}
   echo "Waiting for database on ${METASTORE_DB_HOSTNAME} to launch on ${METASTORE_DB_PORT} ..."
   while ! nc -z ${METASTORE_DB_HOSTNAME} ${METASTORE_DB_PORT}; do
     sleep 1
   done
-
   echo "Database on ${METASTORE_DB_HOSTNAME}:${METASTORE_DB_PORT} started"
-  echo "Init apache hive metastore on ${METASTORE_DB_HOSTNAME}:${METASTORE_DB_PORT}"
-
-  ${HIVE_HOME}/bin/schematool -initSchema -dbType postgres
-  ${HIVE_HOME}/bin/start-metastore
+  /opt/hive/bin/schematool -initSchema -dbType postgres
+  /opt/hive/bin/start-metastore
 fi
